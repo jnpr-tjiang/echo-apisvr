ea7ad561762d17aa034c7f06c8d5ea3fb423d533
Tong Jiang <tjiang@juniper.net>
2020-11-20 14:25:33 -0800

    remove scratch code

--- main.go
+++ main.go
@@ -15,56 +15,8 @@ limitations under the License.
 */
 package main
 
-import (
-	"fmt"
-
-	"github.com/mitchellh/mapstructure"
-)
+import "github.com/jnpr-tjiang/echo-apisvr/cmd"
 
 func main() {
-	// type t struct {
-	// 	N int
-	// }
-	// var n = t{42}
-	// // N at start
-	// fmt.Println(n.N)
-	// // pointer to struct - addressable
-	// ps := reflect.ValueOf(&n)
-	// // struct
-	// s := ps.Elem()
-	// if s.Kind() == reflect.Struct {
-	// 	// exported field
-	// 	f := s.FieldByName("N")
-	// 	if f.IsValid() {
-	// 		// A Value can be changed only if it is
-	// 		// addressable and was not obtained by
-	// 		// the use of unexported struct fields.
-	// 		if f.CanSet() {
-	// 			// change value of N
-	// 			if f.Kind() == reflect.Int {
-	// 				x := int64(7)
-	// 				if !f.OverflowInt(x) {
-	// 					f.SetInt(x)
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// }
-	// // N at end
-	// fmt.Println(n.N)
-	type MyStruct struct {
-		Name string
-		Age  int64
-	}
-
-	myData := make(map[string]interface{})
-	// myData["name"] = "Tony"
-	myData["Age"] = 23
-
-	result := MyStruct{
-		Name: "Tong",
-	}
-	mapstructure.Decode(result, &myData)
-	fmt.Print(result)
-	// cmd.Execute()
+	cmd.Execute()
 }
